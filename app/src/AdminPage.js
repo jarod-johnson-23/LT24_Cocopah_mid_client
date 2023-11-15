3a63ba1214d3919c60373fdb7c267fa5ce852285
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2023-11-15 16:59:00 -0700

    EOD changes

--- app/src/AdminPage.js
+++ app/src/AdminPage.js
@@ -1,10 +1,123 @@
 import "./AdminPage.css";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
+import axios from "axios";
 
 function AdminPage() {
+  const [accessRights, setAccessRights] = useState({});
+  const [email, setEmail] = useState(""); // New state for the email
+
+  // Values for checkboxes
+  const checkboxes = [
+    { label: "Zipcode Heatmap", value: "heatmap" },
+    { label: "Toyota Media Buy", value: "toyota" },
+    { label: "Admin Tool Access", value: "admin" },
+  ];
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setAccessRights((prevState) => {
+      // If the checkbox is checked, add the access right with value true
+      // Otherwise, remove the access right from the object
+      const newState = { ...prevState };
+      if (checked) {
+        newState[name] = true;
+      } else {
+        delete newState[name];
+      }
+      return newState;
+    });
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value); // Update the email state when text is typed
+  };
+
+  const handleSendEmail = async () => {
+    if (!email || Object.keys(accessRights).length === 0) {
+      alert("Please enter an email and select at least one access right.");
+      return;
+    }
+
+    const data = {
+      email: email,
+      accessRights: accessRights,
+    };
+
+    try {
+      // Send a POST request to the /admin/create-user route
+      const response = await axios.post(
+        "http://localhost:5000/admin/create-user",
+        data
+      );
+
+      if (response.status === 201) {
+        alert(
+          "Account creation initiated. A setup email has been sent to the user."
+        );
+      } else {
+        // Handle other successful responses, if any (e.g., 200 OK)
+      }
+    } catch (error) {
+      if (error.response) {
+        // Handle the response error given by the server (e.g., 400 or 409)
+        alert(
+          `Failed to initiate account creation: ${error.response.data.error}`
+        );
+      } else {
+        // Handle connection errors, timeouts, etc.
+        alert("Network or server error. Please try again.");
+      }
+    }
+  };
+
   return (
-    <div className="admin-content">
+    <div className="admin-page">
       <Navbar />
+      <div className="admin-content">
+        <div className="admin-account-section">
+          <h1>Account Section</h1>
+        </div>
+        <div className="vert-line"></div>
+        <div className="admin-create-account-section">
+          <h2>Create Account</h2>
+          <div className="checkbox-group">
+            {checkboxes.map((checkbox) => (
+              <div key={checkbox.value} className="custom-checkbox-wrapper">
+                <input
+                  type="checkbox"
+                  id={`checkbox_${checkbox.value}`}
+                  name={checkbox.value}
+                  onChange={handleCheckboxChange}
+                  checked={accessRights[checkbox.value] === true}
+                />
+                <label
+                  htmlFor={`checkbox_${checkbox.value}`}
+                  className="custom-checkbox"
+                ></label>
+                <label
+                  htmlFor={`checkbox_${checkbox.value}`}
+                  style={{ position: "relative", top: "-4.75px" }}
+                >
+                  {checkbox.label}
+                </label>
+              </div>
+            ))}
+          </div>
+          <div className="text-input-class">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+          <button onClick={handleSendEmail} className="create-btn">
+            Send Email
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- app/src/Login.js
+++ app/src/Login.js
@@ -8,24 +8,55 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // New state for error message
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Validation logic for missing fields
+    if (!email && !password) {
+      setErrorMessage("Please enter an email and password.");
+      return;
+    } else if (!email) {
+      setErrorMessage("Please enter an email.");
+      return;
+    } else if (!password) {
+      setErrorMessage("Please enter a password.");
+      return;
+    }
+
+    // Clear any previous error messages
+    setErrorMessage("");
+
+    // Rest of submit logic
     try {
       const response = await axios.post("http://localhost:5000/user/login", {
         email,
         password,
       });
-      if (response.data.access_token) {
-        localStorage.setItem("token", response.data.access_token);
 
+      if (response.status === 200 && response.data.access_token) {
+        localStorage.setItem("token", response.data.access_token);
         navigate("/dashboard");
-      } else {
-        console.error("Login failed");
       }
     } catch (error) {
-      console.error("Login error:", error);
+      if (error.response) {
+        // Handle the 401 status by checking the response from the server
+        if (error.response.status === 401) {
+          setErrorMessage("Incorrect email or password.");
+        } else {
+          // For other status codes, you can implement additional logic here
+          setErrorMessage(
+            "An error occurred during login. Please try again later."
+          );
+        }
+      } else {
+        console.error("Login error:", error);
+        setErrorMessage(
+          "An error occurred during login. Please try again later."
+        );
+      }
     }
   };
 
@@ -54,7 +85,6 @@ function Login() {
       } catch (error) {
         console.error("Error validating token", error);
         localStorage.removeItem("token");
-      } finally {
       }
     };
 
@@ -64,8 +94,11 @@ function Login() {
   return (
     <div className="login-section">
       <Navbar />
+
       <form onSubmit={handleSubmit} className="login-form">
-        <img src={LTlogo} className="logo-img" />
+        <img src={LTlogo} className="logo-img" alt="LT Logo" />
+        {/* Display an error message if it exists */}
+        {errorMessage && <div className="alert-error">{errorMessage}</div>}
         <div className="input-class">
           <label>Email: </label>
           <input
