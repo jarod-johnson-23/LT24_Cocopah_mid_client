0fe983324da0d0c9fbaa6fa9c687f88c777fc14c
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2023-12-06 09:39:15 -0700

    SOD changes 12/6

--- app/src/BeauJoke.js
+++ app/src/BeauJoke.js
@@ -1,42 +1,45 @@
-import "./BeauJoke.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Typewriter from "./components/Typewriter";
 import axios from "axios";
 import { API_BASE_URL } from "./config";
+import "./BeauJoke.css";
 
 function BeauJoke() {
   const [joke, setJoke] = useState("");
   const [theme, setTheme] = useState("");
-  const [previousJokes, setPreviousJokes] = useState("");
+  const [previousJokes, setPreviousJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchBeauJoke = async () => {
+    setIsLoading(true);
     try {
-      let body = "";
-      if (previousJokes) {
-        body = {
-          theme: theme,
-          responses: {
-            role: "assistant",
-            content: previousJokes.join(" "),
-          },
-        };
-      } else {
-        body = {
-          theme: theme,
-        };
-      }
-      // Logic to fetch the dad joke based on the theme
+      let body =
+        previousJokes.length > 0
+          ? {
+              theme: theme,
+              responses: {
+                role: "assistant",
+                content: previousJokes.join(" "),
+              },
+            }
+          : {
+              theme: theme,
+            };
+
       const response = await axios.post(`${API_BASE_URL}/joke/beau`, body);
 
       if (response.status === 304) {
         alert("The joke was deemed inappropriate, please try again");
+        setIsLoading(false);
         return;
       }
       setJoke(response.data);
       setPreviousJokes((prevJokes) => [...prevJokes, response.data].slice(-10));
     } catch (error) {
       console.error("Error fetching dad joke:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -54,7 +57,9 @@ function BeauJoke() {
         <button onClick={fetchBeauJoke} className="beau-btn">
           Generate Beau Joke
         </button>
+
         <Typewriter text={joke} />
+        {isLoading && <div className="spinner"></div>}
       </div>
     </div>
   );
