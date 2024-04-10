2871d86591e61206d00dcec8e5f3eca09137aeef
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2024-04-10 11:30:39 -0700

    added one stupid slash

--- app/src/AdminPage.js
+++ app/src/AdminPage.js
@@ -99,7 +99,7 @@ function AdminPage() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get(`${API_BASE_URL}/users`);
+        const response = await axios.get(`${API_BASE_URL}/users/`);
         setUsers(response.data);
       } catch (error) {
         console.error("Error fetching users:", error);
