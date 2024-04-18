f0855d57e436f27cfb5eb213ecb470894a37e889
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2024-04-17 17:10:20 -0700

    changes EOD

--- app/src/config.js
+++ app/src/config.js
@@ -1,3 +1,3 @@
-// const API_BASE_URL = "https://py.laneterraleverapi.org";
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://py.laneterraleverapi.org";
+// const API_BASE_URL = "http://127.0.0.1:5000";
 export { API_BASE_URL };
